5eb4286f6a201caec676ef45343f7dc102f57355
rafasYudiShirakura <rafasyudi@gmail.com>
2025-04-08 12:42:59 -0300

    init commit

---
+++ demo/src/main/java/com/EmployeeServiceApplication.java
@@ -0,0 +1,9 @@
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class EmployeeServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeServiceApplication.class, args);
+    }
+}
